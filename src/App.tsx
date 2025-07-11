9ddfd6fda76381d79eec0d6537ad767a332fa44a
Tarik El fouhama <elfouhamatariq@gmail.com>
2025-07-11 08:29:08 +0100

    init  tailwaind

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NavBar from './components/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-black text-white w-full p-10">
+        <NavBar/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+const NavBar = () => {
+    return (
+        <div className="bg-blue-500 px-3 py-2" >NavBar</div>
+    )
+}
+
+export default NavBar
