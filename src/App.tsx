5c8575c507794d8404125b7a4082dd8f4266e9df
Tarik El fouhama <elfouhamatariq@gmail.com>
2025-07-12 15:16:05 +0100

    add public folder  and  fix    tailwaindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import './App.css'
+import Hero from './components/Hero'
 import NavBar from './components/NavBar'
 
 function App() {
-
-
   return (
     <>
-      <div className="bg-black text-white w-full p-10">
-        <NavBar/>
-      </div>
+      <main className="bg-black">
+        <NavBar />
+        <Hero/>
+        Hightlits
+        
+      </main>
     </>
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,24 @@
+import { navLists } from "../constants"
+import { appleImg, bagImg, searchImg } from "../utils/index.js"
+{ }
 const NavBar = () => {
     return (
-        <div className="bg-blue-500 px-3 py-2" >NavBar</div>
+        <header className="w-full py-10 sm:px-10 px-5 flex justify-between items-center">
+            <nav className="flex  w-full  screen-max-width">
+                <img src={appleImg} alt="Apple" width={14} height={18} />
+                <div className="flex flex-1 justify-center  max-sm:hidden "  >
+                    {navLists.map(nav => (
+                        <div className="px-5 text-sm text-gray-500 hover:text-white cursor-pointer transition-all" key={nav}>
+                            {nav}
+                        </div>
+                    ))}
+                </div>
+                <div className="flex items-baseline gap-7 max-sm:flex-1 max-sm:justify-end">
+                    <img src={searchImg} alt="Search" width={18} height={18} />
+                    <img src={bagImg} alt="Bag" width={18} height={18} />
+                </div>
+            </nav>
+        </header>
     )
 }
 
